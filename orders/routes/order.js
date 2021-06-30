1d99f233e6269d6f4edc9e1e9836c868a6a304aa
rajatsaini736 <rajatsaini736@gmail.com>
2021-06-30 22:14:31 +0530

    microservices done

--- orders/routes/order.js
+++ orders/routes/order.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ORDER = require('../models/order');
+const fetch = require('node-fetch');
 
 router.get('/orders', async (req, res) => {
     let response = await ORDER.find();
@@ -11,9 +12,22 @@ router.get('/orders', async (req, res) => {
 
 router.get('/order/:id', async (req, res) => {
     let orderId = req.params.id;
-    let response = await ODER.findByID(orderId);
+    let reponse;
+    response = await ORDER.findById(orderId).lean();
 
     if (!response) response = "no order found";
+
+    let bookId = response.bookId;
+    let customerId = response.customerId;
+
+    let bookResObj = await fetch(`http://localhost:8000/book/${bookId}`);
+    bookResObj = await bookResObj.json();
+    response = Object.assign({}, response, { book: Object.entries(bookResObj).length ? bookResObj.title : null});
+
+    let customerObj = await fetch(`http://localhost:4000/customer/${customerId}`);
+    customerObj = await customerObj.json();
+    response = Object.assign({}, response, { cutomer: Object.entries(customerObj).length ? customerObj.name : null});
+    console.log(response);
     res.send(response);
 });
 
@@ -26,7 +40,8 @@ router.post('/neworder', async (req, res) => {
 
 router.delete('/order/:id', async (req, res) => {
     let orderId = req.params.id;
-    let response = await ORDER.deleteOne({_id: orderId}); 
+    let response = await ORDER.deleteOne({_id: orderId});
+    const fetch = require('node-fetch'); 
 
     res.send(response);
 });
